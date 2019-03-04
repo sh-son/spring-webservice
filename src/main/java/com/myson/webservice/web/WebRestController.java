0b792de646ea11faa612cc05a3f9a17040e41891
pg-sh <tjdgus4722@naver.com>
2019-03-04 17:35:12 +0900

    Entity 및 Repository 생성 및 테스트, LocalDateTime 생성 및 테스트

--- src/main/java/com/myson/webservice/web/WebRestController.java
+++ src/main/java/com/myson/webservice/web/WebRestController.java
@@ -1,13 +1,27 @@
 package com.myson.webservice.web;
 
+import com.myson.webservice.domain.PostsRepository;
+import com.myson.webservice.domain.PostsSaveRequestDto;
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@AllArgsConstructor
 public class WebRestController {
+
+    private PostsRepository postsRepository;
 
     @GetMapping("/hello")
     public String hello() {
         return "HelloWorld";
+    }
+
+    @PostMapping("/posts")
+    public void savePosts(@RequestBody PostsSaveRequestDto dto) {
+        postsRepository.save(dto.toEntity());
+
     }
 }
