4e4181e5e33c9acc7e9e3411892eb1f09989aa63
pg-sh <tjdgus4722@naver.com>
2019-03-07 21:45:18 +0900

    test/ application.yml 추가

--- src/main/java/com/myson/webservice/WebserviceApplication.java
+++ src/main/java/com/myson/webservice/WebserviceApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import java.nio.ByteBuffer;
+
 @EnableJpaAuditing
 @SpringBootApplication
 public class WebserviceApplication {
