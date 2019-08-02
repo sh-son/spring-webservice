92cabc9c9e32bf501d221d41aded7332a319a0f9
shson <tjdgus4722@naver.com>
2019-08-03 03:02:54 +0900

    spring 외부 profile 읽기 및 test api

--- src/main/java/com/myson/webservice/WebserviceApplication.java
+++ src/main/java/com/myson/webservice/WebserviceApplication.java
@@ -1,16 +1,20 @@
 package com.myson.webservice;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-import java.nio.ByteBuffer;
-
-@EnableJpaAuditing
+@EnableJpaAuditing // JPA Auditing 활성화
 @SpringBootApplication
 public class WebserviceApplication {
 
+    public static final String APPLICATION_LOCATIONS = "spring.config.location="
+            + "classpath:application.yml,"
+            + "/app/config/springboot-webservice/real-application.yml";
+
     public static void main(String[] args) {
-        SpringApplication.run(WebserviceApplication.class, args);
+        new SpringApplicationBuilder(WebserviceApplication.class)
+                .profiles(APPLICATION_LOCATIONS)
+                .run(args);
     }
 }
